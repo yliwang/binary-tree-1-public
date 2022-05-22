e9838406d4cfbf381bb024f2b88b073ab2912dd6
yliwang <1220264221@qq.com>
2022-05-22 10:26:39 +0800

    Update leafnode.c

--- leafnode.c
+++ leafnode.c
@@ -4,30 +4,11 @@
 
 #include "tree.h"
 
-void count_leaf_node(struct node *t, void *user)
-{
-	assert(t != NULL);
-
-	int *p = (int *)user;
-	int v = *p;
-	
-
-	if (t->left == NULL && t->right == NULL) {
-		v += 1;
-
-		*p = v;
-	}
-}
-
-
-
 int get_num_leaf(struct binary_tree *tree)
 {
 	int num_leaf = 0;
 
-	pre_order_traversal(tree->root,
-		&num_leaf,
-		count_leaf_node);
+	/* code for counting leaf nodes */
 
 	return num_leaf;
 }
